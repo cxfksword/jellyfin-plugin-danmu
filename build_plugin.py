42362f3cb2907881d8751fcddb820386b771f38a
cxfksword <cxfksword@gmail.com>
2022-10-11 15:29:49 +0800

    Fix github action build

--- build_plugin.py
+++ build_plugin.py
@@ -14,6 +14,7 @@ artifact_dir = os.path.join(os.getcwd(), 'artifacts')
 os.mkdir(artifact_dir)
 
 
+git_version = version
 if version.startswith('v'):
     version = version.lstrip("v")
 version_list = version.split('.')
@@ -27,13 +28,11 @@ if prerelease:
 else:
     jellyfin_repo_file = "./manifest.json"
 
-jellyfin_repo_url = "https://github.com/cxfksword/jellyfin-plugin-danmu/releases/download"
+jellyfin_repo_url = "https://github.com/cxfksword/jellyfin-plugin-danmu/releases/download/%s" % (git_version)
 
 zipfile = os.popen('jprm --verbosity=debug plugin build "." --output="%s" --version="%s" --dotnet-framework="net6.0"' %
                    (artifact_dir, version)).read().strip()
 
 os.system('jprm repo add --url=%s %s %s' % (jellyfin_repo_url, jellyfin_repo_file, zipfile))
-
-os.system('sed -i "s/danmu\//%s\//" %s' % (version, jellyfin_repo_file))
 
 print(version)
