31bf127bfb33f31d07b34ecf10f50388e651fb31
cxfksword <cxfksword@gmail.com>
2022-10-11 14:41:11 +0800

    Update build

---
+++ build_plugin.py
@@ -0,0 +1,36 @@
+import os
+import sys
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--version', required=True)
+parser.add_argument('--prerelease')
+opts = parser.parse_args()
+
+version = opts.version
+prerelease = bool(opts.prerelease)
+
+artifact_dir = os.path.join(os.getcwd(), 'artifacts')
+os.mkdir(artifact_dir)
+
+if prerelease:
+    jellyfin_repo_file = "./manifest-unstable.json"
+    version_list = version.split('.')
+    if len(version_list) == 3:
+        version_list.append('1')
+    else:
+        version_list[3] = str(int(version_list[3]) + 1)
+    version = '.'.join(version_list)
+else:
+    jellyfin_repo_file = "./manifest.json"
+
+jellyfin_repo_url = "https://github.com/cxfksword/jellyfin-plugin-danmu/releases/download"
+
+zipfile = os.popen('jprm --verbosity=debug plugin build "." --output="%s" --version="%s" --dotnet-framework="net6.0"' %
+                   (artifact_dir, version)).read().strip()
+
+os.system('jprm repo add --url=%s %s %s' % (jellyfin_repo_url, jellyfin_repo_file, zipfile))
+
+os.system('sed -i "s/danmu\//%s\//" %s' % (version, jellyfin_repo_file))
+
+print(version)
