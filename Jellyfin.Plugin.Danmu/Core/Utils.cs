bcac10dd2f89acb1f3b7cb77dbaa106d2a172d3d
cxfksword <718792+cxfksword@users.noreply.github.com>
2023-04-16 21:12:45 +0800

    Fix timezone #19

--- Jellyfin.Plugin.Danmu/Core/Utils.cs
+++ Jellyfin.Plugin.Danmu/Core/Utils.cs
@@ -8,10 +8,12 @@ namespace Jellyfin.Plugin.Danmu.Core
 {
     public static class Utils
     {
-        private static TimeZoneInfo beijingTimeZone = TimeZoneInfo.FindSystemTimeZoneById("China Standard Time");
+        // 北京时区
+        static TimeZoneInfo beijingTimeZone = TimeZoneInfo.CreateCustomTimeZone("GMT+8", TimeSpan.FromHours(8), null, null);
 
         public static DateTime UnixTimeStampToDateTime(long unixTimeStamp)
         {
+            // 转成北京时间，bilibili接口的年份只要是根据北京时间返回
             // Unix timestamp is seconds past epoch
             DateTime dateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
             dateTime = TimeZoneInfo.ConvertTime(dateTime.AddSeconds(unixTimeStamp), TimeZoneInfo.Utc, beijingTimeZone);
